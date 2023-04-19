f8d4c3ff823877ad81471b4968289f2e2d3c313a
Dylan COVAREL <dylanco@orange.fr>
2023-04-19 19:45:38 +0200

    route addmatiere test

--- routes/routeAddMatiere.js
+++
@@ -1,9 +0,0 @@
-const express =  require('express');
-const mainCtrl = require('../controllers/ControlAddMatiere.js');
-const router = express.Router() ;
-
-router.get('/connexion', mainCtrl.affichageMenu);
-router.get('/addMatiere', mainCtrl.affichageAddMatiere);
-
-
-module.exports = router 
